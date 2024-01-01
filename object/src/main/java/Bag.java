e38beb78c6ade2631dd3e5631d40916ab99717e5
shkisme <shkisme0130@gmail.com>
2024-01-01 20:15:10 +0900

    [오브젝트] 1장 코드

---
+++ object/src/main/java/Bag.java
@@ -0,0 +1,41 @@
+import java.net.http.HttpRequest.BodyPublisher;
+import java.util.function.BiConsumer;
+
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public Bag(Long amount) {
+        this(amount, null);
+    }
+
+    public Bag(Long amount, Invitation invitation) {
+        this.amount = amount;
+        this.invitation = invitation;
+    }
+
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+}
---
+++ object/src/main/java/TicketOffice.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TicketOffice {
+    private Long amount;
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice(Long amount, Ticket... tickets) {
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    private Ticket getTicket() {
+        return tickets.remove(0);
+    }
+
+    public void sellTicketTo(Audience audience) {
+        plusAmount(audience.buy(getTicket()));
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
